555153f0e3ec265ba44424a46ade2fe654b41943
0x496C6B696E <0x496c6b696e@proton.me>
2026-06-04 03:34:20 +0400

    update

--- Hackerrank/Write_a_function.py
+++ Hackerrank/Write_a_function.py
@@ -1,13 +1,13 @@
-# year = int(input())
+year = int(input())
 
-# def is_leap(year):
-#     if year % 400 == 0:
-#         return True
-#     elif year % 100 == 0:
-#         return False
-#     elif year % 4 == 0:
-#         return True
-#     else:
-#         return False
+def is_leap(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
 
-# print(is_leap(year))
+print(is_leap(year))
