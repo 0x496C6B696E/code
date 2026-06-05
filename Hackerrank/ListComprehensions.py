c78eec83d2b96f21598cc4e806a07b9bbec1e68b
0x496c6b696e <0x496c6b696e@proton.me>
2026-06-05 19:35:19 +0400

    conflict

--- Hackerrank/ListComprehensions.py
+++ Hackerrank/ListComprehensions.py
@@ -1,4 +1,13 @@
-# x = int(input())
-# y = int(input())
-# z = int(input())
-# n = int(input())
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+total = []
+
+for i in range(0, x + 1):
+    for j in range(0, y + 1):
+        for k in range(0, z + 1):
+            if (i + j+ k) != n:
+                total.append([i, j, k])
+print(total)
